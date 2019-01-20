80be171e59fedf2cd23ab2a07ba5adafbdc0b9e3
Georgeana Dias <geanamonte@gmail.com>
2019-01-20 01:07:20 -0200

    Atualização do layout

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  ScrollReveal().reveal('.container', { delay: 100 });
+  ScrollReveal().reveal('header', { delay: 100 });
+  ScrollReveal().reveal('.secao', { delay: 100 });
+  ScrollReveal().reveal('.sidebar', { delay: 100 });
+});
